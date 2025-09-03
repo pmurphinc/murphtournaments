4bff86471575bbbab86db34f103756d5e2172274
pmurphinc <pmurphinc@gmail.com>
2025-09-03 00:09:59 -0700

    Fix ESLint config for Next 14 / ESLint 9

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
+// .eslintrc.cjs
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals'],
-  // Optional: keep it simple. Next 14 works fine with this.
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  extends: ["next/core-web-vitals"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
 };
