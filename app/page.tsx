4765b433890fbdb66ff5c13403668db3c28f9799
pmurphinc <pmurphinc@gmail.com>
2025-09-15 10:27:48 -0700

    adding tournament id

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma';
+import { prisma } from "@/lib/prisma";
 export const revalidate = 30; 
 import { StatusBanner } from "@/components/StatusBanner";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
