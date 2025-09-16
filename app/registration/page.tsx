e8bcb1fe32e4a8365871f0a0547e0ea2ff78b1aa
pmurphinc <pmurphinc@gmail.com>
2025-09-15 20:07:35 -0700

    minor2

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,8 +1,7 @@
 // app/registration/page.tsx
 export const runtime = "nodejs";
 
-import { auth } from "@/lib/auth";
-import { handleRegistration } from "./actions";
 
+export default function RegistrationPage() {
   return <div className="p-8 text-center text-zinc-400">Team registration is now disabled.</div>;
 }
