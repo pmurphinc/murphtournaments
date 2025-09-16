86640bcb5bc793185b322d90187c1fac87005746
pmurphinc <pmurphinc@gmail.com>
2025-09-15 20:06:04 -0700

    minor

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,21 +1,8 @@
 // app/registration/page.tsx
 export const runtime = "nodejs";
 
-import TeamSignupForm from "@/components/signup/TeamSignupForm";
 import { auth } from "@/lib/auth";
 import { handleRegistration } from "./actions";
 
-export default async function RegistrationPage() {
-  const session = await auth();
-
-  return (
-    <div className="max-w-2xl mx-auto p-6">
-      <TeamSignupForm
-        tournaments={[]}
-        captainDiscordName={session?.user?.name ?? ""}
-        userKey={session?.user?.id ?? "anon"}
-        action={handleRegistration}   // <-- pass the server action
-      />
-    </div>
-  );
+  return <div className="p-8 text-center text-zinc-400">Team registration is now disabled.</div>;
 }
